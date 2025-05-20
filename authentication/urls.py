b048f655b58662371a8b36a91c4fcb5bdbafab55
AI-ML-Prowork <pythoncfc@gmail.com>
2025-05-20 15:51:40 +0530

    added guthub runner

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path("user-login", UserLoginView.as_view(), name="login"),
     path("user-logout/", user_logout, name="user_logout"),
 
-    # Google login
-    # path('google-login/', CustomGoogleLogin.as_view(), name='google_login'),
-
 ]
